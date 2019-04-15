51e85aa4a3e9945e22a093e521a02308dc1e7f02
hmyourvillain <49606180+hmyourvillain@users.noreply.github.com>
2019-04-15 12:37:31 +0700

    Create  Exercise5_1_Panai_K.py

---
+++ Exercise5_1_Panai_K.py
@@ -0,0 +1,10 @@
+'''
+คิดเลข
+'''
+print("THIS IS JUST A SIMPLE STUPID CALCULATOR")
+x=float(input("No.1 : "))
+y=float(input("No.2 : "))
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"*",y,"=",x*y)
+print(x,"/",y,"=",x/y)
